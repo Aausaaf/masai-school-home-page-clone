2c07f29679e4c860f9e05e0d5108f8e93bfe2b69
Aausaaf <100550599+Aausaaf@users.noreply.github.com>
2022-06-09 13:13:18 +0530

    Add files via upload

---
+++ masaiSchool Clone/home.js
@@ -0,0 +1,12 @@
+var courselist = document.getElementById("courselist")
+
+function show()
+{
+    
+    courselist.setAttribute("id","shows")
+}
+function hide()
+{
+    
+    courselist.setAttribute("id","showes")
+}
